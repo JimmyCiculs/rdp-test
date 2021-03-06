60423bd94d0e3e6b27f8f89856e5d2c077452d52
JimmyCiculs <55643661+JimmyCiculs@users.noreply.github.com>
2021-03-06 16:22:55 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,8 +1,8 @@
 @echo off
 diskperf -Y >nul
 sc start audiosrv >nul
-net user administrator Tuanx04 /add >nul
-net localgroup administrators administrator /add >nul
+net user Administrator Dat245678 /add >nul
+net localgroup Administrator Administrator /add >nul
 net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP.
 echo IP:
